5df644a36eadd033d4d55de01ac072180942badf
Gefferson-Souza <geffersonteodorodesouza@gmail.com>
2023-03-01 14:01:05 -0300

    second commit

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const blogRouter = require('./controllers/blogController')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
+const middleware = require('./utils/middleware')
+
 mongoose.set('strictQuery', false)
 
 mongoose.connect(config.MONGODB_URI)
@@ -19,4 +21,9 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
-app.use('/api/notes', blogRouter)
+app.use('/api/blog', blogRouter)
+app.use(middleware.morganLogger)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,29 @@
 const morgan = require('morgan')
 const logger = require('./logger')
 
-morgan.token()
+morgan.token('object', (req, res) => {
+    JSON.stringify(req.body)
+})
+
+const morganLogger = morgan(':method :url :status :res[content-length] - :response-time ms :object')
+
+const unknownEndpoint = (req, res) => {
+    res.status(404).json({error: 'Unkown EndingPoint'})
+}
+
+const errorHandler = (err, req, res, next) => {
+    logger.error(err.message)
+
+    const errorStatus = err.status || 400
+
+    res.status(errorStatus).json({error: err.message})
+
+    next(err)
+
+}
+
+module.exports = {
+    morganLogger,
+    unknownEndpoint,
+    errorHandler
+}
